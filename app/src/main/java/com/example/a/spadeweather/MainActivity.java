9804ffd9842839d80b752d51b6ae2264286f3237
LDK <1786079189@qq.com>
2018-06-17 14:55:09 +0800

    Import the HeWeather jar package

---
+++ app/src/main/java/com/example/a/spadeweather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a.spadeweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import interfaces.heweather.com.interfacesmodule.view.HeWeather;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
