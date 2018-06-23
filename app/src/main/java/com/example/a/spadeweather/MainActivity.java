8e6c755504fdce5590e49a17f2696a8778ba5ccf
LDK <1786079189@qq.com>
2018-06-23 17:05:17 +0800

    finish menu lauout

--- app/src/main/java/com/example/a/spadeweather/MainActivity.java
+++ app/src/main/java/com/example/a/spadeweather/MainActivity.java
@@ -1,15 +1,74 @@
 package com.example.a.spadeweather;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import interfaces.heweather.com.interfacesmodule.view.HeWeather;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar title=findViewById(R.id.title);
+        setSupportActionBar(title);
+        mDrawerLayout=findViewById(R.id.drawer_layout);
+        NavigationView navView=findViewById(R.id.nav_view);
+        ActionBar actionBar=getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayShowTitleEnabled(true);
+        }
+        navView.setNavigationItemSelectedListener(new NavigationView.
+                OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_city_manage:
+                        Toast.makeText(MainActivity.this, "city",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_location:
+                        Toast.makeText(MainActivity.this, "location",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_night_mode:
+                        Toast.makeText(MainActivity.this, "night",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_exit:
+                        Toast.makeText(MainActivity.this, "exit",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
