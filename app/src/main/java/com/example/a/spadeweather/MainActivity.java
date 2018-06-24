4e2ba66347c8474cbf83b55700f0084f9b6b3ec5
LDK <1786079189@qq.com>
2018-06-24 14:56:43 +0800

    finish main layout

--- app/src/main/java/com/example/a/spadeweather/MainActivity.java
+++ app/src/main/java/com/example/a/spadeweather/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.a.spadeweather;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.PrimitiveIterator;
 
 import interfaces.heweather.com.interfacesmodule.view.HeWeather;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +37,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar title=findViewById(R.id.title);
         setSupportActionBar(title);
         mDrawerLayout=findViewById(R.id.drawer_layout);
-        NavigationView navView=findViewById(R.id.nav_view);
+        swipeRefreshLayout=findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshWeather();
+            }
+        });
+        final NavigationView navView=findViewById(R.id.nav_view);
         ActionBar actionBar=getSupportActionBar();
         if (actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-            actionBar.setDisplayShowTitleEnabled(true);
         }
         navView.setNavigationItemSelectedListener(new NavigationView.
                 OnNavigationItemSelectedListener() {
@@ -40,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "city",
                                 Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.nav_location:
-                        Toast.makeText(MainActivity.this, "location",
+                    case R.id.nav_refresh:
+                        Toast.makeText(MainActivity.this, "refresh",
                                 Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.nav_night_mode:
@@ -49,14 +69,35 @@ public class MainActivity extends AppCompatActivity {
                                 Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.nav_exit:
-                        Toast.makeText(MainActivity.this, "exit",
-                                Toast.LENGTH_SHORT).show();
+                        final AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("确定退出程序？");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                System.exit(1);
+                            }
+                        });
+                        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                        builder.show();
                         break;
                     default:
                         break;
                 }
                 mDrawerLayout.closeDrawers();
                 return true;
+            }
+        });
+        FloatingActionButton searchButton=findViewById(R.id.search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "search",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -70,5 +111,20 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+    private void refreshWeather(){
+        Toast.makeText(MainActivity.this, "refresh",
+                Toast.LENGTH_SHORT).show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
     }
 }
